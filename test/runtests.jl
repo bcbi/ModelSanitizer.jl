ba5fb196b4332e5c0dfaf35176a71b819f245b97
Dilum Aluthge <dilum@aluthge.com>
2019-07-20 23:32:23 -0400

    Add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,19 @@ using PredictMDSanitizer
 using Test
 
 @testset "PredictMDSanitizer.jl" begin
-    @testset "test-proof-of-concept-dataframes.jl" begin
-        include("test-proof-of-concept-dataframes.jl")
+    @testset "Unit tests" begin
+        @debug("Running unit tests...")
+        @testset "unit-tests/test-sanitize.jl" begin
+            include("unit-tests/test-sanitize.jl")
+        end
     end
-    @testset "test-proof-of-concept-mlj.jl" begin
-        include("test-proof-of-concept-mlj.jl")
+    @testset "Integration tests" begin
+        @debug("Running integration tests...")
+        @testset "integration-tests/test-proof-of-concept-dataframes.jl" begin
+            include("integration-tests/test-proof-of-concept-dataframes.jl")
+        end
+        @testset "integration-tests/test-proof-of-concept-mlj.jl" begin
+            include("integration-tests/test-proof-of-concept-mlj.jl")
+        end
     end
 end
