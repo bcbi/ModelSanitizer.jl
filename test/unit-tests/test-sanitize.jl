ec2e2717b0f1a645d57bfcf4a8ce52ef719b591d
Dilum Aluthge <dilum@aluthge.com>
2019-07-21 00:19:00 -0400

    Update test-sanitize.jl

--- test/unit-tests/test-sanitize.jl
+++ test/unit-tests/test-sanitize.jl
@@ -2,7 +2,7 @@ import PredictMDSanitizer
 import Test
 
 struct Foo end
-Test.@test_throws PredictMDSanitizer._get_property(Foo(), :a)
+Test.@test PredictMDSanitizer._get_property(Foo(), :a) isa Nothing
 Test.@test !PredictMDSanitizer._is_iterable(typeof(1.0))
 Test.@test !PredictMDSanitizer._is_iterable(typeof('a'))
 Test.@test !PredictMDSanitizer._is_iterable(Foo)
