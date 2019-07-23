e159f16ed847ca50276ed6aa14efb89c022457f6
Dilum Aluthge <dilum@aluthge.com>
2019-07-23 08:20:01 -0400

    Update test-sanitize.jl

--- test/unit-tests/test-sanitize.jl
+++ test/unit-tests/test-sanitize.jl
@@ -13,10 +13,10 @@ Test.@test !ModelSanitizer._is_indexable(Foo)
 
 
 
-_sanitize_indexable_with_check_assigned!([1,2,3], ModelSanitizer.Data[], ModelSanitizer._DataElements(0))
-_sanitize_indexable_with_check_assigned!(Foo(), ModelSanitizer.Data[], ModelSanitizer._DataElements(0))
-_sanitize_indexable_without_check_assigned!([1,2,3], ModelSanitizer.Data[], ModelSanitizer._DataElements(0))
-_sanitize_indexable_without_check_assigned!(Foo(), ModelSanitizer.Data[], ModelSanitizer._DataElements(0))
+ModelSanitizer._sanitize_indexable_with_check_assigned!([1,2,3], ModelSanitizer.Data[], ModelSanitizer._DataElements(0))
+ModelSanitizer._sanitize_indexable_with_check_assigned!(Foo(), ModelSanitizer.Data[], ModelSanitizer._DataElements(0))
+ModelSanitizer._sanitize_indexable_without_check_assigned!([1,2,3], ModelSanitizer.Data[], ModelSanitizer._DataElements(0))
+ModelSanitizer._sanitize_indexable_without_check_assigned!(Foo(), ModelSanitizer.Data[], ModelSanitizer._DataElements(0))
 
-_sanitize!(::Foo, data, elements) = error("Cannot sanitize a Foo")
+ModelSanitizer._sanitize!(::Foo, data, elements) = error("Cannot sanitize a Foo")
 ModelSanitizer._sanitize_iterable!([1, 2, Foo()], ModelSanitizer.Data[], ModelSanitizer._DataElements(0))
