1ee4ef84347bceb6e4e2e109ff8a57c2c38c848d
Dilum Aluthge <dilum@aluthge.com>
2019-07-23 03:10:09 -0400

    Progress commit

--- src/utils.jl
+++ src/utils.jl
@@ -10,10 +10,8 @@ function _get_property(m, field)
     return nothing
 end
 
-# alternatively, we could replace `Union{typeof.(original_array)...}` with
-# `typejoin(typeof.(original_array)...)`
-function _fix_vector_type(original_vector::V) where V <: AbstractVector{T} where T
-    new_vector = convert(Vector{Union{typeof.(original_vector)...}}, original_vector)
+function _fix_vector_type(original_vector::AbstractVector)
+    new_vector = vec([original_vector...])
     return new_vector
 end
 
