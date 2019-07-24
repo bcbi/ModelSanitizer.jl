f61b638b40282a74e6eed7e9399fdfc15d536979
Dilum Aluthge <dilum@aluthge.com>
2019-07-23 20:15:18 -0400

    Progress commit

--- test/integration-tests/test-proof-of-concept-linearmodel-1-extra.jl
+++ test/integration-tests/test-proof-of-concept-linearmodel-1-extra.jl
@@ -5,12 +5,46 @@ elements = ModelSanitizer._elements(data)
 
 m = LinearModel{Float64}()
 fit!(m, X[training_rows, :], y[training_rows])
+
+predict(m)
+predict(m, X[training_rows, :])
+predict(m, X[testing_rows, :])
+
 Test.@test predict(m) == predict(m, X[training_rows, :])
 Test.@test mse(m) == mse(m, X[training_rows, :], y[training_rows])
 Test.@test rmse(m) == rmse(m, X[training_rows, :], y[training_rows])
 Test.@test r2(m) == r2(m, X[training_rows, :], y[training_rows])
 
 Test.@test r2(m) > 0.8
+Test.@test r2(m, X[testing_rows, :], y[testing_rows]) > 0.8
+
+Test.@test m.X == X[training_rows, :]
+Test.@test m.y == y[training_rows]
+Test.@test all(m.X .== X[training_rows, :])
+Test.@test all(m.y .== y[training_rows])
+Test.@test !all(m.X .== 0)
+Test.@test !all(m.y .== 0)
+
+sanitize!(Model(m), Data(X), Data(y))
+
+predict(m)
+predict(m, X[training_rows, :])
+predict(m, X[testing_rows, :])
+
+Test.@test predict(m) == predict(m, X[training_rows, :])
+Test.@test mse(m) == mse(m, X[training_rows, :], y[training_rows])
+Test.@test rmse(m) == rmse(m, X[training_rows, :], y[training_rows])
+Test.@test r2(m) == r2(m, X[training_rows, :], y[training_rows])
+
+Test.@test r2(m) > 0.8
+Test.@test r2(m, X[testing_rows, :], y[testing_rows]) > 0.8
+
+Test.@test m.X != X[training_rows, :]
+Test.@test m.y != y[training_rows]
+Test.@test !all(m.X .== X[training_rows, :])
+Test.@test !all(m.y .== y[training_rows])
+Test.@test all(m.X .== 0)
+Test.@test all(m.y .== 0)
 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
