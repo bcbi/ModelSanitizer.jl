479cb19d3b5dca79ff9d71a1463dddd29647db67
Dilum Aluthge <dilum@aluthge.com>
2019-07-25 02:34:27 -0400

    Progress commit

--- benchmark/run.jl
+++ benchmark/run.jl
@@ -1,25 +1,53 @@
 import BenchmarkTools
 import PkgBenchmark
 
-const project_root = dirname(dirname(@__FILE__))
+function run_benchmarks()
+    const project_root = dirname(dirname(@__FILE__))
 
-const proof_of_concept_dataframes = joinpath(project_root, "test", "integration-tests", "test-proof-of-concept-dataframes.jl")
-const proof_of_concept_linearmodel = joinpath(project_root, "test", "integration-tests", "test-proof-of-concept-linearmodel.jl")
-const proof_of_concept_mlj = joinpath(project_root, "test", "integration-tests", "test-proof-of-concept-mlj.jl")
+    const proof_of_concept_dataframes = joinpath(project_root, "test", "integration-tests", "test-proof-of-concept-dataframes.jl")
+    const proof_of_concept_linearmodel = joinpath(project_root, "test", "integration-tests", "test-proof-of-concept-linearmodel.jl")
+    const proof_of_concept_mlj = joinpath(project_root, "test", "integration-tests", "test-proof-of-concept-mlj.jl")
 
-# run each of the scripts once to force compilation of all of the functions
-include(proof_of_concept_dataframes)
-include(proof_of_concept_linearmodel)
-include(proof_of_concept_mlj)
+    # run each of the scripts once to force compilation of all of the functions
+    include(proof_of_concept_dataframes)
+    include(proof_of_concept_linearmodel)
+    include(proof_of_concept_mlj)
 
-const target = "HEAD"
-const baseline = "origin/master-benchmark"
+    const fail_travis_ = true
 
-judgement = BenchmarkTools.judge("ModelSanitizer", target, baseline)
-judgement_suite = PkgBenchmark.benchmarkgroup(judgement)
-for i in ["integration-tests"]
-    for j in ["proof-of-concept-dataframes", "proof-of-concept-linearmodel", "proof-of-concept-mlj"]
+    const fail_travis_time_regression = true
+    const fail_travis_memory_regression = true
+    const target = "HEAD"
+    const baseline = "origin/master-benchmark"
+
+    judgement = BenchmarkTools.judge("ModelSanitizer", target, baseline)
+
+    judgement_suite = PkgBenchmark.benchmarkgroup(judgement)
+    judgement_suite_data = judgement_suite.data
+    for i in ["integration-tests"]
+        judgement_suite_data_i = judgement_suite_data[i]
+        judgement_suite_data_i_data = judgement_suite_data[i].data
+        for j in ["proof-of-concept-dataframes", "proof-of-concept-linearmodel", "proof-of-concept-mlj"]
+            judgement_suite_data_i_data_j = judgement_suite_data_i_data[j]
+        end
     end
 end
 
-const fail_travis_if_benchmarks_detect_performance_regression = true
+run_benchmarks()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if
+else
+end
