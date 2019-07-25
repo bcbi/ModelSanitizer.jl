21d4f45cb1ee2e134db5ed9981ee329ab3a287dc
Dilum Aluthge <dilum@aluthge.com>
2019-07-25 02:37:53 -0400

    Progress commit

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -6,8 +6,8 @@ const proof_of_concept_dataframes = joinpath(project_root, "test", "integration-
 const proof_of_concept_linearmodel = joinpath(project_root, "test", "integration-tests", "test-proof-of-concept-linearmodel.jl")
 const proof_of_concept_mlj = joinpath(project_root, "test", "integration-tests", "test-proof-of-concept-mlj.jl")
 
-const SUITEXXXXXXX = BenchmarkTools.BenchmarkGroup()
-SUITEXXXXXXX["integration-tests"] = BenchmarkTools.BenchmarkGroup()
-SUITEXXXXXXX["integration-tests"]["proof-of-concept-dataframes"] = BenchmarkTools.@benchmarkable include($proof_of_concept_dataframes)
-SUITEXXXXXXX["integration-tests"]["proof-of-concept-linearmodel"] = BenchmarkTools.@benchmarkable include($proof_of_concept_linearmodel)
-SUITEXXXXXXX["integration-tests"]["proof-of-concept-mlj"] = BenchmarkTools.@benchmarkable include($proof_of_concept_mlj)
+const SUITE = BenchmarkTools.BenchmarkGroup()
+SUITE["integration-tests"] = BenchmarkTools.BenchmarkGroup()
+SUITE["integration-tests"]["proof-of-concept-dataframes"] = BenchmarkTools.@benchmarkable include($proof_of_concept_dataframes)
+SUITE["integration-tests"]["proof-of-concept-linearmodel"] = BenchmarkTools.@benchmarkable include($proof_of_concept_linearmodel)
+SUITE["integration-tests"]["proof-of-concept-mlj"] = BenchmarkTools.@benchmarkable include($proof_of_concept_mlj)
