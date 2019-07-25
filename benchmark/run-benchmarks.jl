b3de960bbedfa21a01fb0d04ac2b44e095bff310
Dilum Aluthge <dilum@aluthge.com>
2019-07-25 00:20:17 -0400

    Progress commit

--- benchmark/run-benchmarks.jl
+++ benchmark/run-benchmarks.jl
@@ -1,4 +1,10 @@
 import BenchmarkTools
 import PkgBenchmark
 
-PkgBenchmark.benchmarkpkg("ModelSanitizer"; retune=true)
+# benchmark_result = PkgBenchmark.benchmarkpkg("ModelSanitizer"; retune=true)
+
+const fail_travis_if_benchmarks_detect_performance_regression = true
+const target = "HEAD"
+const baseline = "origin/master-benchmark"
+
+BenchmarkTools.judge("ModelSanitizer", target, baseline)
