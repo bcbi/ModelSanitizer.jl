4754139e216700168ae4989bc968a9bcebb428c9
Dilum Aluthge <dilum@aluthge.com>
2019-07-25 03:19:03 -0400

    Progress commit

--- benchmark/run.jl
+++ benchmark/run.jl
@@ -1,19 +1,18 @@
-import BenchmarkTools
 import PkgBenchmark
 
 function run_benchmarks(project_root = dirname(dirname(@__FILE__));
                         fail_travis_time_regression::Bool = true,
                         fail_travis_memory_regression::Bool = true)
-    proof_of_concept_dataframes = joinpath(project_root, "test", "integration-tests", "test-proof-of-concept-dataframes.jl")
-    proof_of_concept_linearmodel = joinpath(project_root, "test", "integration-tests", "test-proof-of-concept-linearmodel.jl")
-    proof_of_concept_mlj = joinpath(project_root, "test", "integration-tests", "test-proof-of-concept-mlj.jl")
 
-    # run each of the scripts once to force compilation of all of the functions
-    include(proof_of_concept_dataframes)
-    include(proof_of_concept_linearmodel)
-    include(proof_of_concept_mlj)
+    # proof_of_concept_dataframes = joinpath(project_root, "test", "integration-tests", "test-proof-of-concept-dataframes.jl")
+    # proof_of_concept_linearmodel = joinpath(project_root, "test", "integration-tests", "test-proof-of-concept-linearmodel.jl")
+    # proof_of_concept_mlj = joinpath(project_root, "test", "integration-tests", "test-proof-of-concept-mlj.jl")
+    # # run each of the scripts once to force compilation of all of the functions
+    # include(proof_of_concept_dataframes)
+    # include(proof_of_concept_linearmodel)
+    # include(proof_of_concept_mlj)
 
-    judgement = BenchmarkTools.judge("ModelSanitizer", "HEAD", "origin/master-benchmark")
+    judgement = PkgBenchmark.judge("ModelSanitizer", "HEAD", "origin/master-benchmark")
 
     this_judgement_was_failed_for_time = true
     this_judgement_was_failed_for_memory = true
@@ -21,7 +20,7 @@ function run_benchmarks(project_root = dirname(dirname(@__FILE__));
     for i in ["integration-tests"]
         for j in ["proof-of-concept-dataframes", "proof-of-concept-linearmodel", "proof-of-concept-mlj"]
             trial_judgement = PkgBenchmark.benchmarkgroup(judgement).data[i].data[j]
-            if BenchmarkTools.time(trial_judgement) == :regression
+            if PkgBenchmark.time(trial_judgement) == :regression
                 if fail_for_time_regression
                     @error("Fatal time regression detected in $(i)/$(j)")
                     this_judgement_was_failed_for_time = true
@@ -29,7 +28,7 @@ function run_benchmarks(project_root = dirname(dirname(@__FILE__));
                     @error("Non-fatal time regression detected in $(i)/$(j)")
                 end
             end
-            if BenchmarkTools.memory(trial_judgement) == :regression
+            if PkgBenchmark.memory(trial_judgement) == :regression
                 if fail_for_memory_regression
                     @error("Fatal time regression detected in $(i)/$(j)")
                     this_judgement_was_failed_for_memory = true
