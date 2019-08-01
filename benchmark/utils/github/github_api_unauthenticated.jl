c5c7f4fd10efa9df8ff32e07d45cfe875cc547ee
Dilum Aluthge <dilum@aluthge.com>
2019-08-01 03:45:23 -0400

    Add options for authenticating to the GitHub API

--- benchmark/utils/github/github_api_unauthenticated.jl
+++ benchmark/utils/github/github_api_unauthenticated.jl
@@ -9,7 +9,13 @@ function _get_github_pull_request_title_without_number_unauthenticated(travis_pu
     if length(_travis_pull_request) == 0 || _travis_pull_request == "false"
         return ""
     else
-        pull_request_json = HTTP.request("GET", "https://api.github.com/repos/$(github_owner)/$(github_repo)/pulls/$(_travis_pull_request)", ["User-Agent:" => "bcbi-bot"])
+        pull_request_json = HTTP.request(
+            "GET",
+            "https://api.github.com/repos/$(github_owner)/$(github_repo)/pulls/$(_travis_pull_request)",
+            [
+                "User-Agent" => "bcbi-bot",
+                ],
+            )
         pull_request_dict = JSON.parse(String(deepcopy(pull_request_json.body)))
         pull_request_title::String = strip(pull_request_dict["title"])
         return pull_request_title
